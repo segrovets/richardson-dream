c1589023c767bb0ba9ee0a9ab6667707e7bda6eb
segrovets <78635052+segrovets@users.noreply.github.com>
2022-03-18 21:12:37 +0900

    saving progress

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ class microstats():
         self.leader_number= 0
         self.no_other_microbits=0
         self.self_number=0
-
-
 CLASSES are bugged!
 https://forum.makecode.com/t/type-annotation-cannot-appear-on-a-constructor-declaration/4035
 """
 
-me = microstats()
-wait_time = 20e3
+me = {"leader_number":0,
+        "no_other_microbits":0,
+        "self_number":0}
+
+
 current_group = 1
 radio.set_group(current_group)
 
 def fallingBall():
+    wait_time = 20e3
     for i in range(5):
         led.toggle(i,2)
         control.wait_micros(wait_time)
@@ -25,40 +27,38 @@ def fallingBall():
 
 def on_button_pressed_a():
     ## only press button a on leader microbit
-    radio.send_number(me.self_number)
+    radio.send_number(me["self_number"])
 
 def on_button_pressed_b():
-    fallingBall()
-    control.wait_micros(wait_time*6)
-    radio.send_number(current_group)
+    #fallingBall()
+    #control.wait_micros(wait_time*6)
+    basic.show_number(me["self_number"])
 
 def on_received_number(received):
     ## responds to button a press
     ## tells other microbits they are not leader
-    ## others then send 
-    if received == me.leader_number:
+    ## others then send
+    if received == me["leader_number"]:
         #message from leader
-
         basic.show_string("rec."+str(received))
-        me.self_number = 1
-        control.wait_micros(randint(1,1e4)*1e3)
-        basic.show_string("sen.")
-        basic.show_number(me.self_number)
-        radio.send_number(me.self_number)
+        me["self_number"] = randint(1,83)
+        basic.show_string("sen."+str(me["self_number"]))
+        radio.send_number(me["self_number"])
+        radio.set_group(me["self_number"])
 
-    elif me.self_number == me.leader_number and received > me.leader_number:
-        me.no_other_microbits = me.no_other_microbits + 1
-        basic.show_string(str(me.no_other_microbits))
+    elif me["self_number"] == me["leader_number"] and received > me["leader_number"]:
+        me["no_other_microbits"] = me["no_other_microbits"] + 1
+        basic.show_string(str(me["no_other_microbits"]))
     else:
         basic.show_string("else"+str(received))
-        basic.show_string("self"+str(me.self_number))
+        basic.show_string("self"+str(me["self_number"]))
 
 
 def on_received_string(received):
-    # here we can recieve a data packet and 
-    # decompress it into components 
-    # we have all letters and numbers, 
-    # for example certain characters can be used 
+    # here we can recieve a data packet and
+    # decompress it into components
+    # we have all letters and numbers,
+    # for example certain characters can be used
     # to seperate information in the packet.
     return 0
 
@@ -68,3 +68,4 @@ radio.on_received_number(on_received_number)
 
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
-class microstats {
-    leader_number: number
-    no_other_microbits: number
-    self_number: number
-    constructor(): microstats {
-        this.leader_number = 0
-        this.no_other_microbits = 0
-        this.self_number = 0
-    }
-    
+/** 
+class microstats():
+    def __init__(self):
+        self.leader_number= 0
+        self.no_other_microbits=0
+        self.self_number=0
+CLASSES are bugged!
+https://forum.makecode.com/t/type-annotation-cannot-appear-on-a-constructor-declaration/4035
+
+ */
+let me = {
+    "leader_number" : 0,
+    "no_other_microbits" : 0,
+    "self_number" : 0,
 }
 
-let me = new microstats()
-let wait_time = 20e3
 let current_group = 1
 radio.setGroup(current_group)
 function fallingBall() {
+    let wait_time = 20e3
     for (let i = 0; i < 5; i++) {
         led.toggle(i, 2)
         control.waitMicros(wait_time)
@@ -22,17 +25,11 @@ function fallingBall() {
     }
 }
 
-function on_button_pressed_b() {
-    fallingBall()
-    control.waitMicros(wait_time * 6)
-    radio.sendNumber(current_group)
-}
-
 function on_received_string(received: any): number {
-    //  here we can recieve a data packet and 
-    //  decompress it into components 
-    //  we have all letters and numbers, 
-    //  for example certain characters can be used 
+    //  here we can recieve a data packet and
+    //  decompress it into components
+    //  we have all letters and numbers,
+    //  for example certain characters can be used
     //  to seperate information in the packet.
     return 0
 }
@@ -40,25 +37,29 @@ function on_received_string(received: any): number {
 radio.onReceivedNumber(function on_received_number(received: number) {
     // # responds to button a press
     // # tells other microbits they are not leader
-    // # others then send 
-    if (received == me.leader_number) {
+    // # others then send
+    if (received == me["leader_number"]) {
         // message from leader
         basic.showString("rec." + ("" + received))
-        me.self_number = 1
-        control.waitMicros(randint(1, 1e4) * 1e3)
-        basic.showString("sen.")
-        basic.showNumber(me.self_number)
-        radio.sendNumber(me.self_number)
-    } else if (me.self_number == me.leader_number && received > me.leader_number) {
-        me.no_other_microbits = me.no_other_microbits + 1
-        basic.showString("" + me.no_other_microbits)
+        me["self_number"] = randint(1, 83)
+        basic.showString("sen." + ("" + me["self_number"]))
+        radio.sendNumber(me["self_number"])
+        radio.setGroup(me["self_number"])
+    } else if (me["self_number"] == me["leader_number"] && received > me["leader_number"]) {
+        me["no_other_microbits"] = me["no_other_microbits"] + 1
+        basic.showString("" + me["no_other_microbits"])
     } else {
         basic.showString("else" + ("" + received))
-        basic.showString("self" + ("" + me.self_number))
+        basic.showString("self" + ("" + me["self_number"]))
     }
     
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     // # only press button a on leader microbit
-    radio.sendNumber(me.self_number)
+    radio.sendNumber(me["self_number"])
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    // fallingBall()
+    // control.wait_micros(wait_time*6)
+    basic.showNumber(me["self_number"])
 })
