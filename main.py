7b12b293e07c78cf7d701ee9ac65bd83dafd1c43
segrovets <78635052+segrovets@users.noreply.github.com>
2022-03-18 23:07:22 +0900

    micro:bits can uniquely identify themselves, this is logged

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ me = {"leader_number":0,
         "self_number":0}
 
 
+list_devices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83]
+on_network = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 current_group = 1
 radio.set_group(current_group)
 
@@ -27,31 +30,57 @@ def fallingBall():
 
 def on_button_pressed_a():
     ## only press button a on leader microbit
-    radio.send_number(me["self_number"])
+    if me["self_number"] == me["leader_number"]:
+        radio.send_number(me["self_number"])
+    else:
+        basic.show_string("!")
+        # can do something special
 
 def on_button_pressed_b():
     #fallingBall()
     #control.wait_micros(wait_time*6)
-    basic.show_number(me["self_number"])
+    if me["self_number"] == me["leader_number"]:
+        basic.show_string("on net")
+        for i in on_network:
+            if i != 0:
+                basic.show_string("#"+str(i))
+    else:
+        basic.show_string("i'm"+str(me["self_number"]))
 
 def on_received_number(received):
     ## responds to button a press
     ## tells other microbits they are not leader
     ## others then send
-    if received == me["leader_number"]:
-        #message from leader
+    def numberSelf():
         basic.show_string("rec."+str(received))
         me["self_number"] = randint(1,83)
         basic.show_string("sen."+str(me["self_number"]))
+        control.wait_micros(me["self_number"]*1e2)
         radio.send_number(me["self_number"])
-        radio.set_group(me["self_number"])
+    
+    if received == me["leader_number"] and me["self_number"] == me["leader_number"]:
+        #message from leader
+        numberSelf()  
+
+    elif received == me["self_number"]:
+        radio.set_group(me["self_number"]) 
 
     elif me["self_number"] == me["leader_number"] and received > me["leader_number"]:
-        me["no_other_microbits"] = me["no_other_microbits"] + 1
-        basic.show_string(str(me["no_other_microbits"]))
+        if received not in on_network:
+            me["no_other_microbits"] = me["no_other_microbits"] + 1
+            basic.show_string("# m:b="+str(me["no_other_microbits"]))
+            on_network[received-1]=received
+            #send confirmation, you are unique
+            radio.send_number(received)
+        else:
+            radio.set_group(received)
+            radio.send_number(101)
+            radio.set_group(0)
+
+    elif received == 101:
+        numberSelf()
     else:
-        basic.show_string("else"+str(received))
-        basic.show_string("self"+str(me["self_number"]))
+        control.wait_micros(1)
 
 
 def on_received_string(received):
--- main.ts
+++ main.ts
@@ -14,6 +14,8 @@ let me = {
     "self_number" : 0,
 }
 
+let list_devices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83]
+let on_network = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 let current_group = 1
 radio.setGroup(current_group)
 function fallingBall() {
@@ -25,6 +27,7 @@ function fallingBall() {
     }
 }
 
+//  can do something special
 function on_received_string(received: any): number {
     //  here we can recieve a data packet and
     //  decompress it into components
@@ -38,28 +41,61 @@ radio.onReceivedNumber(function on_received_number(received: number) {
     // # responds to button a press
     // # tells other microbits they are not leader
     // # others then send
-    if (received == me["leader_number"]) {
-        // message from leader
+    function numberSelf() {
         basic.showString("rec." + ("" + received))
         me["self_number"] = randint(1, 83)
         basic.showString("sen." + ("" + me["self_number"]))
+        control.waitMicros(me["self_number"] * 1e2)
         radio.sendNumber(me["self_number"])
+    }
+    
+    if (received == me["leader_number"] && me["self_number"] == me["leader_number"]) {
+        // message from leader
+        numberSelf()
+    } else if (received == me["self_number"]) {
         radio.setGroup(me["self_number"])
     } else if (me["self_number"] == me["leader_number"] && received > me["leader_number"]) {
-        me["no_other_microbits"] = me["no_other_microbits"] + 1
-        basic.showString("" + me["no_other_microbits"])
+        if (on_network.indexOf(received) < 0) {
+            me["no_other_microbits"] = me["no_other_microbits"] + 1
+            basic.showString("# m:b=" + ("" + me["no_other_microbits"]))
+            on_network[received - 1] = received
+            // send confirmation, you are unique
+            radio.sendNumber(received)
+        } else {
+            radio.setGroup(received)
+            radio.sendNumber(101)
+            radio.setGroup(0)
+        }
+        
+    } else if (received == 101) {
+        numberSelf()
     } else {
-        basic.showString("else" + ("" + received))
-        basic.showString("self" + ("" + me["self_number"]))
+        control.waitMicros(1)
     }
     
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     // # only press button a on leader microbit
-    radio.sendNumber(me["self_number"])
+    if (me["self_number"] == me["leader_number"]) {
+        radio.sendNumber(me["self_number"])
+    } else {
+        basic.showString("!")
+    }
+    
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     // fallingBall()
     // control.wait_micros(wait_time*6)
-    basic.showNumber(me["self_number"])
+    if (me["self_number"] == me["leader_number"]) {
+        basic.showString("on net")
+        for (let i of on_network) {
+            if (i != 0) {
+                basic.showString("#" + ("" + i))
+            }
+            
+        }
+    } else {
+        basic.showString("i'm" + ("" + me["self_number"]))
+    }
+    
 })
